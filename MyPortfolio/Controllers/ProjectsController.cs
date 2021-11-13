050a98545e196af9456df05ef2af3e5c866aa7f6
Зиля Шагиахметова <asunauki98@mail.ru>
2021-11-13 10:40:44 +0300

    Добавьте файлы проекта.

---
+++ MyPortfolio/Controllers/ProjectsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio.Controllers
+{
+    public class ProjectsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var projects = ProjectStorage.Projects;
+            return View(projects);
+        }
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Project project)
+        {
+            ProjectStorage.Add(project);
+            return RedirectToAction("Index");
+        }
+        
+    }
+}
---
+++ MyPortfolio/Core/ProjectStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyPortfolio
+{
+    public class ProjectStorage
+    {
+        public static List<Project> Projects { get; private set; } = new List<Project>
+        {
+            new Project
+            {
+                Name = "КинокрадHD",
+                Link = "https://github.com/Zilya23/KinokradHD",
+                Description = "No description"
+            }
+        };
+        public static void Add(Project project)
+        {
+            Projects.Add(project);
+        }
+
+
+    }
+}
